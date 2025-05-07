c989e22036de2e632dd823756d5a9d58139d5f6d
Sebastian Ospina <asebasg07@gmail.com>
2025-05-07 14:23:43 -0500

    Ejercicio 3

---
+++ src/ejercicio3.js
@@ -0,0 +1,28 @@
+const persona = {
+  nombre: "Sebastian",
+  edad: 18,
+  ciudad: "Medellin",
+};
+
+console.log(
+  "Primera persona identificada: ",
+  persona.nombre,
+  "Edad: ",
+  persona.edad,
+  "Ciudad: ",
+  persona.ciudad
+);
+
+// Ahora se van a cambiar los atributos de la persona
+persona.nombre = "Maria";
+persona.edad = 15;
+persona.ciudad = "Cali";
+
+console.log(
+    "Segunda persona identificada: ",
+    persona.nombre,
+    "Edad: ",
+    persona.edad,
+    "Ciudad: ",
+    persona.ciudad
+    );
