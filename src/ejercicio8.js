6832ef86faed441f088c8aa5510750bbf7f6505a
Sebastian Ospina <asebasg07@gmail.com>
2025-05-07 16:51:14 -0500

    Ejercicios 8 y 9

--- src/ejercicio8.js
+++ src/ejercicio8.js
@@ -6,10 +6,10 @@ let edad = parseInt(edadStr);
 let tieneCarnetStr = prompt("¿Tiene carnet de conducir? (si/no):");
 let tieneCarnet = tieneCarnetStr.toLowerCase() === "si";
 let estaInhabilitadoStr = prompt("¿Está inhabilitado para conducir? (si/no):");
-let estaInhabilitado = estaInhabilitadoStr.toLowerCase() === "si";
+let estaInhabilitado = estaInhabilitadoStr.toLowerCase() === "no";
 
 function puedeConducir(edad, tieneCarnet, estaInhabilitado) {
-  if (edad >= 18 && tieneCarnet && !estaInhabilitado) {
+  if (edad >= 18 && tieneCarnet && estaInhabilitado) {
     return true;
   }
   return false;
---
+++ src/ejercicio9.js
@@ -0,0 +1,16 @@
+// Reescribe el siguiente if-else usando el operador ternario
+/*
+if (edad >= 18) {
+    return "Puede votar";
+} else {
+    return "No puede votar";
+}
+*/
+
+if (edad >= 18) {
+    console.log("Puede votar");
+} else {
+    console.log("No puede votar");
+}
+
+// TERMINAR CODIGO
